f45afb4b50998c5fcf87541f7d63dedd0d7817cd
codrin18 <codrin1999@gmail.com>
2021-05-19 12:29:21 +0300

    Added some backEnd but is not working

---
+++ backEnd/src/main/java/com/cms/controllers/InitializationController.java
@@ -0,0 +1,55 @@
+package main.java.com.cms.controllers;
+
+import com.cms.dto.token.TokenDto;
+import com.cms.dto.token.TokenInformation;
+import com.cms.dto.user.LoginUserDto;
+import com.cms.dto.user.RegisterUserDto;
+import com.cms.dto.user.UserDto;
+import com.cms.dto.user.UserInformationDto;
+import com.cms.services.InitializationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping
+public class InitializationController {
+
+    @Autowired
+    private InitializationService initializationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDto> register(@RequestBody RegisterUserDto registerUserDto) {
+
+        UserDto userDto = initializationService.register(registerUserDto);
+        return new ResponseEntity<>(userDto, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody LoginUserDto loginUserDto) {
+        TokenDto login = initializationService.login(loginUserDto);
+        return ResponseEntity.ok(login);
+    }
+
+    @GetMapping("/user-information")
+    public ResponseEntity<UserInformationDto> getUserInformation(@RequestParam(required = false) String conferenceId) {
+        return ResponseEntity.ok(initializationService.getUserInformation(conferenceId));
+    }
+
+    @GetMapping("/token-information")
+    public ResponseEntity<TokenInformation> getTokenInformation() {
+        return ResponseEntity.ok(initializationService.getTokenInformation());
+    }
+
+    @PostMapping("/invitation/{invitationId}")
+    public ResponseEntity<UserDto> registerUserByInvitation(@PathVariable String invitationId,
+                                                            @RequestBody RegisterUserDto registerUserDto) {
+        UserDto userDto = initializationService.completeInvitation(invitationId, registerUserDto);
+        return new ResponseEntity<>(userDto, HttpStatus.CREATED);
+    }
+
+}
+
+
---
+++ backEnd/src/main/java/com/cms/controllers/UsersController.java
@@ -0,0 +1,33 @@
+package main.java.com.cms.controllers;
+
+import com.cms.dto.user.UserDto;
+import com.cms.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private UsersService usersService;
+
+    /**
+     * Returns the list with all users from the database.
+     * @return list of Users as JSON, plus status code 200
+     */
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getUsers() {
+        return ResponseEntity.ok(usersService.getUsers());
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<UserDto> getUser(@PathVariable String username) {
+        return ResponseEntity.ok(usersService.getUserDto(username));
+    }
+}
+
